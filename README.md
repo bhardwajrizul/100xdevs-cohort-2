831aa581e5906b54d527fcbca6f68ed69e83fdcf
bhardwajrizul <bhardwajrizul9816@gmail.com>
2023-12-14 02:48:28 +0530

    [Pass] NodeJS test pass

--- README.md
+++ README.md
@@ -1,17 +1,28 @@
 # Cohort 2 Assignment Solutions 
 
 ## WEEK 2 
+* Learning Material Updated ✅
 * Code with explanation ✅
 * All test cases running ✅
-  
-[![tests.gif](https://i.postimg.cc/Qt2v14JN/tests.gif)](https://postimg.cc/tZzrHzc8)
  
 #### Learning Material for week 2
 * Watch the lectures - https://app.100xdevs.com/
 * Learn everything about Promises in JS (Coding Train Playlist) - https://youtube.com/playlist?list=PLRqwX-V7Uu6bKLPQvPRNNE65kBL62mVfx&si=rBC2UjvYn0xj9zeX 
 * Callback function v/s Promise - https://www.youtube.com/watch?v=ranuTFXPgbw
+* Working with File System Module ('fs') - https://youtu.be/yQBw8skBdZU?si=uQu7BoUILZL1wRC9
+* Creating a Node server using Express - https://youtu.be/SccSCuHhOw0?si=4Xj-i5ZXulqHFwt8
+* How to use Postman - https://youtu.be/wmz1sGZp814?si=p3EsV4bKz_1uvx8k
 
 ##### Extras
 * Understanding Event Loop - https://www.youtube.com/watch?v=8aGhZQkoFbQ
 * NodeJS docs on event loop (***Advanced***) - https://nodejs.org/en/guides/event-loop-timers-and-nexttick/
 
+---
+
+- [x] 01-async-js
+
+[![tests.gif](https://i.postimg.cc/Qt2v14JN/tests.gif)](https://postimg.cc/tZzrHzc8)
+
+- [x] 02-nodejs
+  
+[![test-fileserver.png](https://i.postimg.cc/90bP56Kn/test-fileserver.png)](https://postimg.cc/H8J8bhpw)
---
+++ week-2/02-nodejs/fileServer.js
@@ -0,0 +1,67 @@
+/**
+  You need to create an express HTTP server in Node.js which will handle the logic of a file server.
+  - Use built in Node.js `fs` module
+  The expected API endpoints are defined below,
+  1. GET /files - Returns a list of files present in `./files/` directory
+    Response: 200 OK with an array of file names in JSON format.
+    Example: GET http://localhost:3000/files
+  2. GET /file/:filename - Returns content of given file by name
+     Description: Use the filename from the request path parameter to read the file from `./files/` directory
+     Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
+     Example: GET http://localhost:3000/file/example.txt
+    - For any other route not defined in the server return 404
+    Testing the server - run `npm run test-fileServer` command in terminal
+ */
+
+// Run the app locally using `npm run fileServer` command in terminal 
+// Uncomment the app.listen() code when running locally
+
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+
+const FILES_DIRECTORY = path.join(__dirname, 'files');
+
+
+// get a list of files
+app.get('/files', (req, res) => {
+  fs.readdir(FILES_DIRECTORY, (err, files) => {  // readdir reads the contents of a directory
+    if (err) {
+      return res.status(500).send('Internal Server Error');
+    }
+
+    res.status(200).json(files);
+  });
+});
+
+// get the contents of a specific file
+app.get('/file/:filename', (req, res) => {
+  const filename = req.params.filename;
+  const filePath = path.join(FILES_DIRECTORY, filename);
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        return res.status(404).send('File not found');
+      }
+      return res.status(500).send('Internal Server Error');
+    }
+
+    // File content found
+    res.status(200).send(data);
+  });
+});
+
+// Invalid Routes
+app.all('*', (req, res) => {
+  res.status(404).send('Route not found');
+});
+
+// When running locally uncomment the below code, when testing keep it commented
+/* 
+  app.listen(3001, () => {
+   console.log('Server started on port 3001');
+  });
+*/
+module.exports = app;
